493ac566f2feb516708e2ab2cb69ba1bc03377fb
GrassetRepo <alexandrecfgalindo@gmail.com>
2022-11-29 18:30:26 -0500

    commit 2

--- src/hwJava.java
+++ src/hwJava.java
@@ -3,6 +3,16 @@ public class hwJava {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println();
+		
+		int soma;
+		int mat;
+		
+		soma = 16;
+		mat = soma * 5;
+		
+		soma = mat;
+		System.out.println(soma);
+		
 	}
 
 }
